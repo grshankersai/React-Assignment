9a26859dad7e656402bd8e3c49209ea490dc33d2
Shanker <grshankersai@gmail.com>
2022-02-09 15:25:33 +0530

    09-Feb-2022 3:25pm :: Testing-5

--- src/components/molecules/CustomSearch/__Test__/CustomSearch.test.js
+++ src/components/molecules/CustomSearch/__Test__/CustomSearch.test.js
@@ -11,3 +11,15 @@ it("Check if custom search is rendered",()=>{
     expect(ele).toBeInTheDocument();
 })
 
+it("Check-1",()=>{
+    render(<CustomSearch/>);
+    const ele1 = screen.getByTestId('icon-btn');
+    expect(ele1).toBeInTheDocument();
+})
+
+it("Check-2",()=>{
+    render(<CustomSearch/>);
+    const ele2 = screen.getByTestId('search-btn');
+    expect(ele2).toBeInTheDocument();
+})
+
